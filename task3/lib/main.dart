ee91dcc7cc771fb70c8ea90593f296895a73c927
hasanalbanna <banna15-5064@diu.edu,bd>
2025-04-09 03:17:45 +0600

    done

---
+++ task3/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ListView Example',
+      home: const NumberListScreen(),
+    );
+  }
+}
+
+class NumberListScreen extends StatelessWidget {
+  const NumberListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Generate a list of numbers from 1 to 100
+    final List<int> items = List<int>.generate(100, (i) => i + 1);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Number List'),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Item ${items[index]}'),
+          );
+        },
+      ),
+    );
+  }
+}

2af021fa1fd00dbc8d9990c1c3cc3f695d58ccb7
hasanalbanna <banna15-5064@diu.edu,bd>
2025-04-09 02:58:41 +0600

    first commit

---
+++ task1/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Lobster', 
+      ),
+      home: const HelloWorldScreen(),
+    );
+  }
+}
+
+class HelloWorldScreen extends StatelessWidget {
+  const HelloWorldScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.deepPurple, 
+          ),
+        ),
+      ),
+    );
+  }
+}
